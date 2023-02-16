e032f5e682297940fbed4773070874293d79b944
Jackson Socolofsky <jtsoco@gmail.com>
2023-02-16 14:06:25 +0900

    add more movies to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
+puts "clearing bookmarks"
+Bookmark.destroy_all
 puts "clearing movie database"
 Movie.destroy_all
 puts "creating true movies"
@@ -14,8 +16,8 @@ Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s f
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 puts "creating random movies"
-10.times do
-  Movie.create!(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://source.unsplash.com/random/?movie/?sig=#{rand(100)}", rating: rand(10).to_f)
+20.times do
+  Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: "https://source.unsplash.com/random/?movie/?sig=#{rand(100)}", rating: rand(10).to_f)
 end
 
 puts "finished!"
