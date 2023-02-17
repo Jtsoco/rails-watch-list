faa22d1d551613be1971fdc3623c1d63f69c7a1f
Jackson Socolofsky <jtsoco@gmail.com>
2023-02-17 16:41:28 +0900

    better css

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only: [:index, :new, :create, :show] do
+  resources :lists, only: [:new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
